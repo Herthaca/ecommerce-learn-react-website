523b8db4c3bd0e57f6a099960fbd7bfc86b92ac8
Herthaca <imranjalidah96@gmail.com>
2026-06-16 22:06:43 +0800

    Added home page

---
+++ src/pages/Auth.jsx
@@ -0,0 +1,5 @@
+export default function Auth() {
+  return <div className="auth">
+    <h1>Auth</h1>
+  </div>
+}
---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,5 @@
+export default function Checkout() {
+  return <div className="checkout">
+    <h1>Checkout</h1>
+  </div>
+}
